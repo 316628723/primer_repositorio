3f83af181a6126e4cee2c8d4219253c447071a8f
Karin Uno <54488499+karin-uno99@users.noreply.github.com>
2019-11-11 10:08:56 -0600

    correcciones

--- TAREAS/8/main.c
+++ TAREAS/8/main.c
@@ -3,23 +3,23 @@
 
 int main(int argc, char *argv[]){
 	//declarar variales 
-	float x;
-	float y;
-	float z;
+	double x;
+	double y;
+	double z;
 	//convertir argumentos a flotantes
 	x=atof(argv[1]);
 	y=atof(argv[2]);
 	//de grados farenheit a celcius, sera 1
-	if(x==1){
+	if(x!=0){
 		z=(y-32)/1.8;
-		printf("%.2f\n",z);
+		printf("%.2lf\n",z);
 		return 0;
 	}
 	//de grados celcius a farenheit, sera 0
 	else if(x==0){
 		z=32+(y*1.8);
-		printf("%.2f\n",z);
-		return 0:
+		printf("%.2lf\n",z);
+		return 0;
 	}
 return 0;
 }
