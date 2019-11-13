52152b8c71494dd4c522fe4817c9e5e80a1cdb56
Karin Uno <54488499+karin-uno99@users.noreply.github.com>
2019-11-12 23:01:57 -0600

    tarea11

--- TAREAS/11/main.c
+++ TAREAS/11/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-double raiz(double n,double t,double c){
+double raiz(double n,double t,double c){ //las variables de entrada n, la tolerancia y candidato
 double prom;
 double div;
-while (fabs(div-c)>t){
+while (fabs(div-c)>t){ //se le resta division a candidato 
 	div=n/c;
-	prom=(div+c)/2;
+	prom=(div+c)/2;// el promedio de la division y el candidato
 	c=prom;
 }
 long double res=c;
 return res;
 }
-int main(int arg, char*argv[]){
+int main(int arg, char*argv[]){ //va repitiendo el mismo procedimiento con los nuevos valores de la variable 
 	double x=atof(argv[1]);
 	double y=atof(argv[2]);
 	double z=atof(argv[3]);
